a627cbfc89d0848dc6913bb85b5c39f156aaca79
Peter <peter@oazon.com>
2017-06-27 21:56:40 +0800

    atoi

---
+++ java/src/atoi.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2017/6/27 0027.
+ */
+public class atoi {
+    public int myAtoi(String str){
+        if (str==null||str.equals("")){
+            return Integer.MIN_VALUE;
+        }
+        char[] chars = str.toCharArray();
+
+    }
+    public static void main(String[] args) {
+        char a='或';
+        int b=a;
+        System.out.println(b);
+    }
+}
