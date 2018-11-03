9390e4e94ed4fc2158889680585bcac18e8ec9e1
liberty2015 <409947972@qq.com>
2018-11-04 01:24:55 +0800

    add roman-to-integer

--- src/RomanToInt.java
+++ src/RomanToInt.java
@@ -1,6 +1,68 @@
+/**
+ * https://leetcode-cn.com/problems/roman-to-integer/
+ * 13. Roman to Integer
+ */
+
 public class RomanToInt {
 
     public int romanToInt(String s) {
+        char[] chars = s.toCharArray();
+        int ret = 0;
+        char prev=' ';
+        for (int i = 0; i < chars.length; i++) {
+            char ch=chars[i];
+            if (ch=='I'){
+                ret+=1;
+            }else if (ch=='V'){
+                if (prev=='I'){
+                    ret--;
+                    ret+=4;
+                }else {
+                    ret+=5;
+                }
+            }else if (ch=='X'){
+                if (prev=='I'){
+                    ret--;
+                    ret+=9;
+                }else {
+                    ret+=10;
+                }
+            }else if (ch=='L'){
+                if (prev=='X'){
+                    ret-=10;
+                    ret+=40;
+                }else {
+                    ret+=50;
+                }
+            }else if (ch=='C'){
+                if (prev=='X'){
+                    ret-=10;
+                    ret+=90;
+                }else {
+                    ret+=100;
+                }
+            }else if (ch=='D'){
+                if (prev=='C'){
+                    ret-=100;
+                    ret+=400;
+                }else {
+                    ret+=500;
+                }
+            }else if (ch=='M'){
+                if (prev == 'C') {
+                    ret-=100;
+                    ret+=900;
+                }else {
+                    ret+=1000;
+                }
+            }
 
+            prev=ch;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new RomanToInt().romanToInt("MCMXCIV"));
     }
 }
